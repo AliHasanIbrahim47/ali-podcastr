61813607ea8a6433bd6a099175ecb7a7623bb32d
AliHasanIbrahim47 <ew9361903@gmail.com>
2024-09-18 14:17:09 +0300

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://mighty-ferret-15.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true
+    },
+    images: {
+        remotePatterns: [
+            {
+            protocol: 'https',
+            hostname: 'lovely-flamingo-139.convex.cloud'
+            },
+            {
+            protocol: 'https',
+            hostname: 'amicable-swan-517.convex.cloud'
+            },
+            {
+            protocol: 'https',
+            hostname: 'img.clerk.com'
+            },
+        ]
+    }
+};
 
 export default nextConfig;
